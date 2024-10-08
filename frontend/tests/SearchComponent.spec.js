b518a7602571167cf0fd6511bf5459b803c6e33f
Atong Abraham Kur <107772819+Abram-MrRight@users.noreply.github.com>
2024-10-08 12:46:43 +0300

    Update SearchComponent.spec.js

--- frontend/tests/SearchComponent.spec.js
+++ frontend/tests/SearchComponent.spec.js
@@ -1,5 +1,5 @@
 import { shallowMount } from '@vue/test-utils';
-import SearchComponent from '../components/SearchComponent.vue';
+import SearchComponent from '@/components/SearchComponent.vue';
 
 
 describe('SearchComponent.vue', () => {
