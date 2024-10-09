d7758d960ff96bab1ffea71486a519933a56b549
Iraku Harry <iraqooh@gmail.com>
2024-10-09 16:29:40 +0300

    MySQL service fix

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -1,7 +1,7 @@
 const db_config = {
-    host: process.env.DB_HOST || 'localhost',
+    host: process.env.DB_HOST || 'mysql',
     user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'kaspastinski',
+    password: process.env.DB_PASSWORD || '',
     database: process.env.DB_NAME || 'cyber_school',
     dialect: process.env.DB_DIALECT || 'mysql',
     port: process.env.DB_PORT || 3306,
